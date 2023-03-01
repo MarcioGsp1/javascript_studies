3bc804b7daf5dad7abf676079d46579e30fe7c8c
Marcio Silva <marciocinema28064212@gmail.com>
2023-03-01 13:39:16 -0300

    basic of functions

--- fundamentals01/app.js
+++ fundamentals01/app.js
@@ -503,3 +503,160 @@ HINT: Value X is between 50 and 300, if it's >= 50 && <= 300
 const bill = 430;
 const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
 console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
+
+
+
+
+
+'use strict';
+/*
+'use strict' é uma funcionalidade do JavaScript que altera o modo de execução do código, tornando-o mais seguro e com menos chances de ocorrer erros e comportamentos inesperados.
+
+Quando o 'use strict' é utilizado no início de um arquivo ou de uma função, o código passa a ser executado em modo restrito, em que algumas funcionalidades do JavaScript são desativadas ou têm o comportamento alterado. Isso ajuda a prevenir erros comuns e a promover boas práticas de programação.
+*/
+
+
+///////////////////////////////////////
+// Functions
+function logger() {
+  console.log('My name is Márcio');
+}
+
+// calling 
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+
+///////////////////////////////////////
+// Function Declarations vs. Expressions
+
+// Function declaration
+function calcAge1(birthYear) {
+  return 2023 - birthYear;
+}
+const age1 = calcAge1(1998);
+
+// Function expression
+const calcAge2 = function (birthYear) {
+  return 2023 - birthYear;
+}
+const age2 = calcAge2(1998);
+
+console.log(age1, age2);
+
+
+///////////////////////////////////////
+// Arrow functions
+
+const calcAge3 = birthYear => 2023 - birthYear;
+const age3 = calcAge3(1998);
+console.log(age3);
+
+const $yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2023 - birthYear;
+  const retirement = 65 - age;
+  // return retirement;
+  return `${firstName} retires in ${retirement} years`;
+}
+
+console.log($yearsUntilRetirement(1998, 'Márcio')); console.log($yearsUntilRetirement(1980, 'Bob'));
+
+
+///////////////////////////////////////
+// Functions Calling Other Functions
+function cutFruitPieces(fruit) {
+  return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
+
+  const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
+  return juice;
+}
+console.log(fruitProcessor(4, 9));
+
+
+///////////////////////////////////////
+// Reviewing Functions
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+}
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+  const age = calcAge(birthYear);
+  const retirement = 65 - age;
+
+  if (retirement > 0) {
+    console.log(`${firstName} retires in ${retirement} years`);
+    return retirement;
+  } else {
+    console.log(`${firstName} has already retired 🎉`);
+    return -1;
+  }
+}
+
+console.log(yearsUntilRetirement(1991, 'Márcio'));
+console.log(yearsUntilRetirement(1950, 'Mike'));
+
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/*
+Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new gymnastics discipline, which works differently.
+Each team competes 3 times, and then the average of the 3 scores is calculated (so one average score per team).
+A team ONLY wins if it has at least DOUBLE the average score of the other team. Otherwise, no team wins!
+
+1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
+2. Use the function to calculate the average for both teams
+3. Create a function 'checkWinner' that takes the average score of each team as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner to the console, together with the victory points, according to the rule above. Example: "Koalas win (30 vs. 13)".
+4. Use the 'checkWinner' function to determine the winner for both DATA 1 and DATA 2.
+5. Ignore draws this time.
+
+TEST DATA 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
+TEST DATA 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
+
+HINT: To calculate average of 3 values, add them all together and divide by 3
+HINT: To check if number A is at least double number B, check for A >= 2 * B. Apply this to the team's average scores 😉
+
+*/
+
+
+const calcAverage = (a, b, c) => (a + b + c) / 3;
+//console.log(calcAverage(3, 4, 5));
+
+// Test 1
+let $scoreDolphins = calcAverage(44, 23, 71);
+let $scoreKoalas = calcAverage(65, 54, 49); 
+console.log($scoreDolphins, $scoreKoalas);
+
+const checkWinner = function (avgDolphins, avgKoalas) {
+  if (avgDolphins >= 2 * avgKoalas) {
+    console.log(`Dolphins win 🏆 (${avgDolphins} vs. ${avgKoalas})`);
+  } else if (avgKoalas >= 2 * avgDolphins) {
+    console.log(`Koalas win 🏆 (${avgKoalas} vs. ${avgDolphins})`);
+  } else {
+    console.log('No team wins...');
+  }
+}
+checkWinner($scoreDolphins, $scoreKoalas);
+
+
+// Test 2
+/* $scoreDolphins = calcAverage(85, 54, 41);
+$scoreKoalas = calcAverage(23, 34, 27);
+ */
